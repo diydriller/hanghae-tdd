5459af5e6dc82cd033e575a5e492fb36d60ae98d
diydriller <dhrhd080@naver.com>
2024-12-15 02:13:26 +0900

    feat: add feature to get user point history list - implemented controller method to handle GET request for user point history list - add service method to get user point history list from database

--- src/main/kotlin/io/hhplus/tdd/point/PointService.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointService.kt
@@ -1,13 +1,19 @@
 package io.hhplus.tdd.point
 
+import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import org.springframework.stereotype.Service
 
 @Service
 class PointService(
-    private val userPointTable: UserPointTable
+    private val userPointTable: UserPointTable,
+    private val pointHistoryTable: PointHistoryTable
 ) {
     fun getUserPoint(userId: Long): UserPoint {
         return userPointTable.selectById(userId)
+    }
+
+    fun getUserPointHistory(userId: Long): List<PointHistory> {
+        return pointHistoryTable.selectAllByUserId(userId)
     }
 }
